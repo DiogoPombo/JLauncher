4c381c321851041af731fef42b3abe4d8d4bffb1
Diogo Santos Pombo <digo_pombo@hotmail.com>
2026-06-22 08:45:30 -0300

    Adicionado Trigger on Boot e Launcher para desktop

---
+++ JLS/LaunchApp.cmd
@@ -0,0 +1,53 @@
+@echo off
+chcp 1252 >nul
+setlocal
+
+java -version >nul 2>&1
+if not %errorlevel%==0 (
+    COLOR 0C
+    echo ============================================================
+    echo [ERROR] Java not found.
+    echo Please install Java before running JLauncher.
+    echo ============================================================
+    powershell -NoProfile -Command "(New-Object -ComObject WScript.Shell).Popup('Plataforma Java não detectada! É nescessário instalar o Java para executar este aplicativo!', 0, 'Livro Caixa', 48)"
+    timeout /t 5 /nobreak >nul
+    goto end
+)
+
+:MAIN
+:: Configurações
+set "BASE=%~dp0"
+set "JAVA_FILE_PATH=%BASE%..\target\file.jar"
+set "APP_URL=http://localhost:8080/"
+
+netstat -ano | findstr /R /C:":8080 .*LISTENING" >nul 2>&1
+
+if %errorlevel% equ 0 (
+    goto :OPEN_BROWSER
+) else (
+    goto :SHOW_ALERT
+)
+
+:OPEN_BROWSER
+:: Verifica a existência do Edge no Registro para aplicar o Modo APP ou usar o Fallback
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe" >nul 2>&1
+if %errorlevel% equ 0 (
+    :: Abre o Edge em modo APP
+    start msedge --app=%APP_URL%
+) else (
+    :: Fallback para o navegador padrão do sistema
+    start %APP_URL%
+)
+goto :END
+
+:SHOW_ALERT
+:: Comando 1: Executa exclusivamente o efeito sonoro de exclamação do Windows
+::powershell -NoProfile -Command "[System.Media.SystemSounds]::Exclamation.Play()"
+
+:: Comando 2: Executa exclusivamente a caixa de diálogo (via objeto COM para evitar erros de tipo)
+powershell -NoProfile -Command "(New-Object -ComObject WScript.Shell).Popup('LIVRO CAIXA ainda não está pronto, tente novamente em alguns instantes!', 0, 'Livro Caixa', 48)"
+
+start "" javaw -jar "%JAVA_FILE_PATH%"
+
+:END
+exit /b
---
+++ JLS/StartTrigger.cmd
@@ -0,0 +1,27 @@
+@echo off
+chcp 1252 >nul
+setlocal
+
+java -version >nul 2>&1
+if not %errorlevel%==0 (
+    COLOR 0C
+    echo ============================================================
+    echo [ERROR] Java not found.
+    echo Please install Java before running JLauncher.
+    echo ============================================================
+    powershell -NoProfile -Command "(New-Object -ComObject WScript.Shell).Popup('Plataforma Java não detectada! É nescessário instalar o Java para executar este aplicativo!', 0, 'Livro Caixa', 48)"
+    timeout /t 5 /nobreak >nul
+    goto end
+)
+
+
+
+:: Configurações
+set "BASE=%~dp0"
+set "JAVA_FILE_PATH=%BASE%..\target\file.jar"
+set "APP_URL=http://localhost:8080/"
+
+start "" javaw -jar "%JAVA_FILE_PATH%"
+
+endlocal
+exit
